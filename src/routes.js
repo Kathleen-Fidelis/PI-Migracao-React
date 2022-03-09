f86e72a3e98e27aaf39a13c89f0dcecec392793e
isabotasso <isabellabotasso20@gmail.com>
2022-03-09 14:54:26 -0300

    migracao

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import { Switch, Route } from "react-router-dom";
+import Home from './pages/home/Home'
+import Product from './pages/product/Product'
+import Cart from './pages/cart/Cart'
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import AssessmentsHome from "./components/assessmentsHome/AssessmentsHome";
+
+
+export const Routes = () => {
+    return (
+        <Switch>
+            <Route path="/" component={Home} exact/>
+            <Route path="/header" component={Header} exact/>
+            <Route path="/product" component={Product} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/nav" component={Nav} exact/>
+            <Route path="/assessmentsHome" component={AssessmentsHome} exact/>
+            
+
+        </Switch>
+    )
+}
