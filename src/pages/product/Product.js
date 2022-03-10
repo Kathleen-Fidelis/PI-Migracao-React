8223c742aa43e1168f2bf96fb686a9fbcaf2bf4a
Kathleen <kathleenfidelis12@gmail.com>
2022-03-10 18:12:01 -0300

    página produto

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -4,49 +4,42 @@ import Footer from '../../components/footer/Footer'
 import FreightBanner from '../../components/freightBanner/FreightBanner'
 import ProductBanner from '../../components/productBanner/ProductBanner'
 import Pagination  from '../../components/pagination/Pagination'
-import Card from '../../components/card/Card'
-import CardP from '../../components/card/CardProduct'
-import Product1 from '../../components/card/imgs/heart (2).png'
+import CardP from '../../components/cardProduct/CardProduct'
+import emilly from './imgs/emilly.jpg'
+import bruna from './imgs/bruna.jpg'
+import bianca from './imgs/bianca1.jpg'
+import alice from './imgs/alice.jpg'
+import mayara from './imgs/mayara.jpg'
+import renata from './imgs/renata.jpg'
+import joelma from './imgs/joelma.jpg'
+import camila from './imgs/camila.jpg'
 
-function Product() {
+
+function Product(props) {
     return (
         <>
             <FreightBanner />
             <Header />
             <ProductBanner />
+            
             <div className="row nomePagina">
                 <h2>MENINAS</h2>
             </div>
 
-            {/*<div className="cards">
-                <div className="container-card">
-                    <ul className="lista-produtos">
-                        <li className="item-produto">
-                            <div className="fav">
-                                <a className="fav-icon"><img src="./imgs/heart (2).png" width="25px" height="25px" /></a>
-                                <br />
-                            </div>
-                            <img src="../paginas/imgs/2.jpg" className="imagem-produto" width="215" height="215" />
-                            <br />
-                            <div className="nome-produto">Bebê reborn Emilly</div>
+            <div className="cards">
+                <CardP Image={ emilly } Name="Bebê Reborn Emilly" price="619" parcel="61,90"/>
+                <CardP Image={ bruna } Name="Bebê Reborn Bruna" price="499" parcel="49,90"/>
+                <CardP Image={ bianca } Name="Bebê Reborn Bianca" price="599" parcel="59,90"/>
+                <CardP Image={ alice } Name="Bebê Reborn Alice" price="659" parcel="65,90"/>
+            </div>
 
-                            <div className="precos">
-                                <div className="preco">R$ 619,00</div>
-                                <div className="parcelado">OU 10X DE R$ 61,90</div>
-                            </div>
-
-                            <button type="button" className="comprar"><a href="../paginas/PaginaItem2.html">Comprar </a></button>
-
-                        </li>
-                    </ul>
-                </div>
-            </div>*/}
-
-
-
-            <Card productName="Bebê Reborn Mayara" imgProduct="./imgs/heart (2).png" valueProduct=" R$ 799,00 "  parcel=" Ou 10x sem juros"/>
-           <Card productName="Bebê Reborn Mayara" valueProduct=" R$ 799,00 "  parcel=" Ou 10x sem juros"/>
-           <CardP Image={ Product1 }/>
+            <div className="cards">
+                <CardP Image={ mayara } Name="Bebê Reborn Mayara" price="799" parcel="79,90"/>
+                <CardP Image={ renata } Name="Bebê Reborn Renata" price="659" parcel="65,90"/>
+                <CardP Image={ joelma } Name="Bebê Reborn Joelma" price="619" parcel="61,90"/>
+                <CardP Image={ camila } Name="Bebê Reborn Camila" price="459" parcel="45,90"/>
+            </div>
+           
 
             <Pagination />
             {/*<Footer/>*/}
