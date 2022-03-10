d2170e689e56bd73012ce016a991248fb84304d0
Kaique Araujo <kaiquematosdearaujo@hotmail.com>
2022-03-10 11:12:57 -0300

    Update 2.0

---
+++ src/pages/profile/Profile.js
@@ -0,0 +1,18 @@
+import './Profile.css'
+import Container from '../../components/profileContainer/ProfileContainer'
+
+
+
+function Profile() {
+    return (
+
+        <>
+      <Container/>
+
+        </>
+
+    )
+
+}
+
+export default Profile
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,16 @@ import Home from './pages/home/Home'
 import Product from './pages/product/Product'
 import Cart from './pages/cart/Cart'
 import ItemPage from './pages/itemPage/ItemPage'
+import Profile from './pages/profile/Profile'
 
 export const Routes = () => {
     return (
         <Switch>
-            <Route path="/" component={Home} exact/>
+            <Route path="/" component={Home} exact />
             <Route path="/product" component={Product} />
             <Route path="/cart" component={Cart} />
             <Route path="/itemPage" component={ItemPage} />
+            <Route path="/profile" component={Profile} />
         </Switch>
     )
 }
