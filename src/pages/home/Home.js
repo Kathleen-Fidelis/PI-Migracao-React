8cab0ca93f5ebcd8d4cbc4ba18077122c8e76b80
Kaique Araujo <kaiquematosdearaujo@hotmail.com>
2022-03-11 08:59:41 -0300

    update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,22 @@ import './Home.css'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
 import Avaliation from '../../components/assessmentsHome/AssessmentsHome'
+import CarrouselBanner from '../../components/bannerCarrousel/BannerCarrousel'
+import BannerSecondary from '../../components/bannerSecondary/BannerSecondary'
+import Categories from '../../components/categories/Categories'
+
+
+
 function Home() {
     return (
         <>
             <Header/>
+            <CarrouselBanner/>
+            <BannerSecondary/>
+            <Categories/>
+            <p className=""/>
             <Avaliation/>
-            <Footer/>
+            <Footer />
         </>
     )
 }
