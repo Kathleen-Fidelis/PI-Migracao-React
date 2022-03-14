c4953b249af32ab87f43d1b26d199a3fecd9cfe4
Kaique Araujo <kaiquematosdearaujo@hotmail.com>
2022-03-14 10:27:55 -0300

    teste

--- src/routes.js
+++ src/routes.js
@@ -25,6 +25,7 @@ import Login from './pages/login/Login'
 export const Routes = () => {
     return (
         <Switch>
+            
             <Route path="/" component={Home} exact />
             <Route path="/product" component={Product} />
             <Route path="/cart" component={Cart} />
